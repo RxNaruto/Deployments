bfe5544bef7ec57cbb41e8f908ed2131c5d4d300
Rithk <rithkchaudhary86@gmail.com>
2024-06-02 18:26:59 +0530

    hono middlewares

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -2,7 +2,17 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.post('/', async(c) => {
+async function authMiddleware(c: any,next: any){
+  if(c.req.header("Authorization")){
+    //do Validation
+    await next();
+  }else{
+    return c.text("You dont have access");
+  }
+}
+
+
+app.post('/', authMiddleware,async(c) => {
   const body = await c.req.json();
   console.log(body);
   console.log(c.req.header("Authorization"));
